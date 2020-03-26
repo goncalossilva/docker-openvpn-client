0b882786b39ec6256a98a36504d19a3630be51a8
Gonçalo Silva <goncalossilva@gmail.com>
2020-03-26 15:56:40 +0000

    Remove kill switch

--- README.md
+++ README.md
@@ -1,13 +1,11 @@
-# OpenVPN Client for Docker
-## Why?
+# OpenVPN Client with proxy for Docker
+
 Using this image will allow you establish a VPN connection usable by other containers without having to install a VPN client on the host. The image requires the user to supply the needed OpenVPN configuration files, so (probably) any VPN provider will work.
 
-It has a VPN killswitch, so if the VPN connection is lost at any time, all internet connectivity to this and connected containers is lost.
-
-Please note that the killswitch does allow connections to the VPN server address/port combinations specified in the configuration file in order to establish connection.
-
 ## Creating
+
 ### `docker run`
+
 ```
 docker run -d \
   --name=openvpn-client \
@@ -18,6 +16,7 @@ docker run -d \
 ```
 
 ### `docker-compose`
+
 ```
 version: '2'
 
@@ -33,45 +32,49 @@ services:
             - <path/to/config>:/data/vpn
         restart: unless-stopped
 ```
+
 #### Considerations
+
 ##### Tinyproxy
-If enabling Tinyproxy, you'll want to publish port 8888 to access the proxy. To do that, add `-p 8888:8888` if you're using `docker run`, or add the below snippet to the `openvpn-client` service definition in your Compose file if using `docker-compose`.
+
+To use Tinyproxy, you'll want to publish port 8888 to access the proxy. To do that, add `-p 8888:8888` if you're using `docker run`, or add the below snippet to the `openvpn-client` service definition in your Compose file if using `docker-compose`.
+
 ```
 ports:
     - 8888:8888
 ```
 
 ##### Handling ports intended for connected containers
+
 If you plan on having [other containers use `openvpn-client`'s network stack](#using-with-other-containers) and those containers have web UIs, you'll want to publish the web UI ports on `openvpn-client` instead of the connected container. To do that, add `-p <host_port>:<container_port>` if you're using `docker run`, or add the below snippet to the `openvpn-client` service definition in your Compose file if using `docker-compose`.
+
 ```
 ports:
     - <host_port>:<container_port>
 ```
+
 In both cases, replace `<host_port>` and `<container_port>` with the port used by your connected container.
 
 ### Environment variables
 
 | Variable | Default (blank is unset) | Description |
 | --- | --- | --- |
-| `FORWARDED_PORTS` | | Port(s) forwarded by your VPN provider (e.g. `12345` or `9876,54321`) |
 | `LOG_LEVEL` | `3` | OpenVPN verbosity (`1`-`11`) |
-| `SUBNETS` | | A comma-separated (no whitespaces) list of LAN subnets (e.g. `192.168.0.0/24,192.168.1.0/24`) |
-| `TINYPROXY` | | The on/off status of the forward proxy; to enable, set to `on`. Any other value, including leaving it unset, will cause the proxy to not start. |
 | `TINYPROXY_PORT` | `8888` | The port that Tinyproxy listens on. If manually specified, choose a port over 1024. |
 | `TINYPROXY_USER` | | Setting `TINYPROXY_USER` and `TINYPROXY_PASS` will restrict access to the proxy server to only the specified username and password. |
 | `TINYPROXY_PASS` | | Setting `TINYPROXY_USER` and `TINYPROXY_PASS` will restrict access to the proxy server to only the specified username and password. |
 
-#### `SUBNETS`
-The subnets specified will have routes created and whitelists added in the firewall for them which allows for connectivity to and from hosts on the subnets. 
-
 ## Running
+
 ### Verifying functionality
+
 Once you have container running `yacht7/openvpn-client`, run the following command to spin up a temporary container using `openvpn-client` for networking. The `wget -qO - ifconfig.me` bit will return the public IP of the container (and anything else using `openvpn-client` for networking). You should see an IP address owned by your VPN provider.
 ```
 docker run --rm -it --network=container:openvpn-client alpine wget -qO - ifconfig.me
 ```
 
 ### Using with other containers
+
 Once you have your OpenVPN client container up and running, you can tell other containers to use `openvpn-client`'s network stack which gives any container the ability to utilize the VPN tunnel. There are a few ways to accomplish this depending how how your container is created.
 
 If your container is being created with
--- data/entry.sh
+++ data/entry.sh
@@ -15,7 +15,6 @@ if ! $(echo $LOG_LEVEL | grep -Eq '^([1-9]|1[0-1])$'); then
 fi
 
 echo -e "\n---- Details ----"
-echo "Whitelisting subnets: ${SUBNETS:-none}"
 echo "Using configuration file: $config_file"
 echo "Using OpenVPN log level: $LOG_LEVEL"
 
@@ -46,102 +45,35 @@ echo "[INFO] Changes made and files moved into place."
 
 ################################################################################
 
-echo -e "\n---- Network and Firewall ----"
-local_subnet=$(ip r | grep -v 'default via' | grep eth0 | tail -n 1 | cut -d " " -f 1)
-default_gateway=$(ip r | grep 'default via' | cut -d " " -f 3)
-
-echo "Creating VPN kill switch and local routes."
-
-# allows established and related connections
-iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-
-# allows loopback connections
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
-
-# allows Docker network connections
-iptables -A INPUT -s $local_subnet -j ACCEPT
-iptables -A OUTPUT -d $local_subnet -j ACCEPT
-
-# for every specified subnet...
-for subnet in ${SUBNETS//,/ }; do
-    # create a route to it and...
-    ip route add $subnet via $default_gateway dev eth0
-    # allow connections
-    iptables -A INPUT -s $subnet -j ACCEPT
-    iptables -A OUTPUT -d $subnet -j ACCEPT
-done
-
-# allows OpenVPN connections only to addresses in configuration file
-remote_port=$(grep "port " $config_file | cut -d " " -f 2)
-remote_proto=$(grep "proto " $config_file | cut -d " " -f 2 | cut -c1-3)
-remotes=$(grep "remote " $config_file | cut -d " " -f 2-4)
-
-echo "$remotes" | while IFS= read line; do
-    domain=$(echo "$line" | cut -d " " -f 1)
-    port=$(echo "$line" | cut -d " " -f 2)
-    proto=$(echo "$line" | cut -d " " -f 3 | cut -c1-3)
-    for ip in $(nslookup $domain localhost | tail -n +4 | grep -Eo '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}' | sort | uniq); do
-        echo $domain $ip $port $proto
-        iptables -A OUTPUT -o eth0 -d $ip -p ${proto:-$remote_proto} --dport ${port:-$remote_port} -j ACCEPT
+# start list of commands to run Tinyproxy
+# https://www.gnu.org/software/bash/manual/html_node/Command-Grouping.html
+{
+    echo "[INFO] Running tinyproxy"
+    # Wait for VPN connection to be established
+    while ! ping -c 1 1.1.1.1 > /dev/null 2&>1; do
+        sleep 1
     done
-done
 
-# allow connections over VPN interface...
-iptables -A INPUT -i tun0 -j ACCEPT
-iptables -A OUTPUT -o tun0 -j ACCEPT
+    addr_eth=$(hostname -i)
+    addr_tun=$(ip a show dev tun0 | grep inet | cut -d " " -f 6 | cut -d "/" -f 1)
+    TINYPROXY_PORT=${TINYPROXY_PORT:-8888}
 
-# and over VPN interface to forwarded ports
-if [ ! -z $FORWARDED_PORTS ]; then
-    for port in ${FORWARDED_PORTS//,/ }; do
-        if [ $port -lt 1024 ] || [ $port -gt 65535 ]; then
-            echo "[WARN] $port not a valid port. Ignoring."
+    sed -i \
+        -e "/Port/c Port $TINYPROXY_PORT" \
+        -e "/Listen/c Listen $addr_eth" \
+        -e "/Bind/c Bind $addr_tun" \
+        /etc/tinyproxy/tinyproxy.conf
+
+    if [ ! -z $TINYPROXY_USER ]; then
+        if [ ! -z $TINYPROXY_PASS ]; then
+            echo -e "\nBasicAuth $TINYPROXY_USER $TINYPROXY_PASS" >> /etc/tinyproxy/tinyproxy.conf
+        else
+            echo "[WARN] Tinyproxy username supplied without password. Starting without credentials."
         fi
-        iptables -A INPUT -i tun0 -p tcp --dport $port -j ACCEPT
-        iptables -A INPUT -i tun0 -p udp --dport $port -j ACCEPT
-    done
-fi
+    fi
 
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
-
-echo "[INFO] iptables rules created and routes configured."
-
-################################################################################
-
-if [ "$TINYPROXY" = "on" ]; then
-    # start list of commands to run Tinyproxy
-    # https://www.gnu.org/software/bash/manual/html_node/Command-Grouping.html
-    {
-        echo "[INFO] Running tinyproxy"
-        # Wait for VPN connection to be established
-        while ! ping -c 1 1.1.1.1 > /dev/null 2&>1; do
-            sleep 1
-        done
-
-        addr_eth=$(hostname -i)
-        addr_tun=$(ip a show dev tun0 | grep inet | cut -d " " -f 6 | cut -d "/" -f 1)
-        TINYPROXY_PORT=${TINYPROXY_PORT:-8888}
-
-        sed -i \
-            -e "/Port/c Port $TINYPROXY_PORT" \
-            -e "/Listen/c Listen $addr_eth" \
-            -e "/Bind/c Bind $addr_tun" \
-            /etc/tinyproxy/tinyproxy.conf
-
-        if [ ! -z $TINYPROXY_USER ]; then
-            if [ ! -z $TINYPROXY_PASS ]; then
-                echo -e "\nBasicAuth $TINYPROXY_USER $TINYPROXY_PASS" >> /etc/tinyproxy/tinyproxy.conf
-            else
-                echo "[WARN] Tinyproxy username supplied without password. Starting without credentials."
-            fi
-        fi
-
-        tinyproxy -c /etc/tinyproxy/tinyproxy.conf
-    } &
-fi
+    tinyproxy -c /etc/tinyproxy/tinyproxy.conf
+} &
 
 cd /etc/openvpn
 
